4cca3af09e374455205180994aa3e185417362e8
dimsen <dimas.10119285@mahasiswa.unikom.ac.id>
2021-10-14 15:08:22 +0700

    First Commit

---
+++ src/ifelse/Main.java
@@ -0,0 +1,12 @@
+package ifelse;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Index index = new Index();
+
+        index.input();
+        index.index(index.value);
+        index.output();
+    }
+}
